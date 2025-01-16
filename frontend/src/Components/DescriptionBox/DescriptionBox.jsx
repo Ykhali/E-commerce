6f3705c5f51f9abe7a5af50a14346f9d14a83752
Ykhali <Youssef.Khalid@emsi-edu.ma>
2025-01-16 21:41:22 +0100

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          possimus voluptate expedita culpa, adipisci iusto iure laborum harum
+          reprehenderit est id exercitationem sed quisquam asperiores iste alias
+          ducimus neque voluptatibus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Praesentium corrupti, excepturi quod, repellat
+          quibusdam culpa alias pariatur adipisci esse temporibus obcaecati,
+          perspiciatis deleniti. Quos perferendis a veritatis repellat, maxime
+          quidem.
+        </p>
+        <p>
+            
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
